1196f35e03c09b3fde0ac2efb20104dab05b69ed
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-14 10:55:46 +0100

    Header of the home page is done

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import './assets/style/sass/style.scss';
 import Navbar from './components/Navbar';
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Home />
       </div>
     </Router>
   );
---
+++ front/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Header_img from '../assets/images/header_img.webp';
+
+const Header = () => {
+    return(
+        <header className="header-container">
+            <div className="header-content">
+                <h1 className="heading-1 header-heading">Always the ride you want</h1>
+                <p className="paragraph paragraph--mr-b">Request a ride, hop in, and relax.</p>
+                <Link className="btn-black btn--rect-m">Sign up to ride</Link>
+            </div>
+            <div>
+                <img src={Header_img} alt="Two girls waiting for a cab."/>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
