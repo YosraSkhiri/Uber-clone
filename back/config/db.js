e13db101c7f84a50eecc8efab5704b86fc1a2cf6
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-16 14:55:16 +0100

    fixed some bugs in the dbConnect function and used async await with mongoose connect

--- back/config/db.js
+++ back/config/db.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-mongoose.connect(
-    `mongodb://${config.get(dbConfig.host)}:${config.get(dbConfig.port)}/${config.get(dbConfig.dbName)}`, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true, 
-        useCreateIndex: true
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb://${config.get('dbConfig.host')}:${config.get('dbConfig.port')}/${config.get('dbConfig.dbName')}`, 
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true, 
+                useCreateIndex: true
+            }
+        );
+        console.log('mongoDB connected');
+    }catch(error) {
+        console.log(error);
     }
-);
+    
+}
+
+module.exports = dbConnect;
