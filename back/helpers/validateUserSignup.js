560c9af5a2881cc330716086b1f9c69c455aae0f
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-16 18:14:52 +0100

    added a helper function to validate the user signup input

--- back/helpers/validateUserSignup.js
+++ back/helpers/validateUserSignup.js
@@ -1,17 +1,17 @@
-exports.validateUserSignup = (signupData) => {
+validateUserSignup = (signupData) => {
     let {
         firstname,
         lastname,
         email,
         phone,
-        city,
+        state,
         password,
         role
     } = signupData;
 
     let validationErrors = [];
 
-    if(!firstname || !lastname || !email || !phone || !city || !password || !role) {
+    if(!firstname || !lastname || !email || !phone || !state || !password || !role) {
         if(!firstname) {
             validationErrors.push('Firstname is required.');
         }
@@ -28,8 +28,8 @@ exports.validateUserSignup = (signupData) => {
             validationErrors.push('Phone number is required.');
         }
 
-        if(!city) {
-            validationErrors.push('City is required.');
+        if(!state) {
+            validationErrors.push('State is required.');
         }
 
         if(!password) {
@@ -41,27 +41,47 @@ exports.validateUserSignup = (signupData) => {
         }
     }
 
-    if(firstname.length < 3) {
+    if(firstname && firstname.length < 3) {
         validationErrors.push('Your firstname must have at leat 3 characters.');
     }
 
-    if(lastname.length < 3) {
+    if(lastname && lastname.length < 3) {
         validationErrors.push('Your lastname must have at leat 3 characters.');
     }
 
     const regularExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(!regularExp.test(email)) {
+    if(email && !regularExp.test(email)) {
         validationErrors.push('Please enter a valid email.');
     }
 
-    if(phone.length !== 8) {
+    if(phone && phone.length !== 8) {
         validationErrors.push('Your phone number is less than 8 numbers.');
     }
 
     const validPhoneinitials = ['9', '2', '3', '5' , '4'];
-    if(!validPhoneinitials.includes(phone.substring(0, 1))) {
+    if(phone && !validPhoneinitials.includes(phone.substring(0, 1))) {
         validationErrors.push('Please enter a valid phone number.');
+    }
+    
+    if(password && password.length < 8) {
+        validationErrors.push('Please enter a longer password (min 8 characters).');
+    }
+
+    if(role && role !== 'rider' && role !== 'driver') {
+        validationErrors.push('Please choose a rider account or a driver account.');
+    }
+
+    const tunisianStates = ['Ariana', 'Beja', 'Ben Arous', 'Bizerte', 'Gabes', 
+                            'Gafsa', 'Jendouba', 'Kairouan', 'Kasserine', 
+                            'Kebili', 'Kef', 'Mahdia', 'Manouba', 'Medenine', 
+                            'Monastir', 'Nabeul', 'Sfax', 'Sidi Bouzid', 'Siliana', 
+                            'Sousse', 'Tataouine', 'Tozeur', 'Tunis', 'Zaghouan'];
+
+    if(state && !tunisianStates.includes(state)) {
+        validationErrors.push('Please choose a valid state.');
     }
 
     return validationErrors;
 }
+
+module.exports = validateUserSignup;
