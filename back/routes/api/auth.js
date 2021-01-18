d48e6aa6953e77e1c47938fe6c69b1d93c7d20a7
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-18 23:34:16 +0100

    added the login route

--- back/routes/api/auth.js
+++ back/routes/api/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authContr = require('../../controllers/auth');
 
 router.post('/signup', authContr.signup);
+router.post('/login', authContr.login);
 
 module.exports = router;
