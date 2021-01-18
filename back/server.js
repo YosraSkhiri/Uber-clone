1377ad79afd01011aa9a1b12b310d6aafc4daa49
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-18 23:35:13 +0100

    added the cookie parser package

--- back/server.js
+++ back/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const dbConnect = require('./config/db');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const authRoute = require('./routes/api/auth');
 
 dbConnect();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use('/api/auth/', authRoute);
 
