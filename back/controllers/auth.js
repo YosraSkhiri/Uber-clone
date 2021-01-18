1e7d1b679f0f08896958589d3b4e910724fc2acb
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-18 23:33:15 +0100

    added the login controller

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -1,7 +1,9 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const validateUserSignupData = require('../helpers/validateUserSignup');
+const config = require('config');
+const validateUserSignup = require('../helpers/validateUserSignup');
+const validateUserLogin = require('../helpers/validateUserLogin');
 
 module.exports = {
     signup: async (req, res) => {
@@ -17,7 +19,7 @@ module.exports = {
         } = req.body;
 
 
-        const validationResults = validateUserSignupData(signUpData);
+        const validationResults = validateUserSignup(signUpData);
 
         if(validationResults.length > 0) {
             return res.status(400).json({ errors: validationResults });
@@ -40,6 +42,43 @@ module.exports = {
 
         } catch(error) {
             return res.status(500).json({ errors: ['A server error has accured, Please try again later.']})
+        }
+    },
+
+    login: async (req, res) => {
+        let loginData = {
+            email,
+            password,
+            role
+        } = req.body;
+
+        const validationResults = validateUserLogin(loginData);
+
+        if(validationResults.length > 0) {
+            return res.status(400).json({ errors: validationResults });
+        }
+
+        try {
+            const user = await User.findOne({ email, role });
+            if(!user) {
+                return res.status(401).json({msg: ['Please enter the right email and the right password.']});
+            }
+            
+            const userId = user._id;
+            const passwordCompareResult = await bcrypt.compare(password, user.password);
+            
+            if(passwordCompareResult){
+                const token = await jwt.sign({ userId }, config.get('jwtPrivateKey'));
+                return res.status(200)
+                          .cookie('loginToken', token, { httpOnly: true })
+                          .json({ msg: ['Logged in successfully.'] });  
+            }
+
+            return res.status(400).json({ msg: ['You entered the wrong password or the wrong email.'] });
+               
+
+        } catch(error) {
+            return res.status(500).json({ errors: ['A server error has accured, Please try again later.']});
         }
     }
 }
