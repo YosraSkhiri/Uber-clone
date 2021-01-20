fa6f3ff8070572c5d407f2e519fe7573121db07b
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-20 19:50:22 +0100

    added redux thunk

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -1,9 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import allReducers from './reducers';
+import ReduxThunk from 'redux-thunk';
 
 let store = createStore(
     allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+        applyMiddleware(ReduxThunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 export default store;
