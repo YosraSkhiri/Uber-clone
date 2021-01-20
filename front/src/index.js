4834cf4eaf45d4c8a83bdb2c467d6773f620af5d
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-20 19:39:23 +0100

    I moved the store from src/index.js to its own file

--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
-import allReducers from './redux/reducers';
 import { Provider } from 'react-redux';
-
-let store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={ store }>
