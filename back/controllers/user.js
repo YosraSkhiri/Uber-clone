00380cf521b7ebbb5d71c9475dd9fd110eb44657
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-22 00:44:00 +0100

    added the update user route

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,10 +1,34 @@
 const User = require('../models/User');
+const validateUserUpdate = require('../helpers/validateUserUpdate');
 
 module.exports = {
     getUser: async (req, res) => {
         try {
             const user = await User.findById(req.user);
             return res.status(200).json(user);
+        } catch(error) {
+            return res.status(500).json({ errors: ['A server error has accured, Please try again later.']});
+        }
+    },
+
+    updateUser: async (req, res) => {
+        let newUserData = {
+            firstname,
+            lastname,
+            email,
+            phone,
+            state,
+        } = req.body;
+
+        const validationResults = validateUserUpdate(newUserData);
+
+        if(validationResults.length > 0) {
+            return res.status(400).json({ errors: validationResults });
+        }
+
+        try {
+            await User.updateOne({ _id: req.user }, newUserData);
+            return res.status(200).json({ msg: ['Your information has been updated!']});
         } catch(error) {
             return res.status(500).json({ errors: ['A server error has accured, Please try again later.']});
         }
--- back/routes/api/user.js
+++ back/routes/api/user.js
@@ -4,5 +4,6 @@ const userContr = require('../../controllers/user');
 const auth = require('../../middlewares/auth');
 
 router.get('/me', auth, userContr.getUser);
+router.post('/update', auth, userContr.updateUser);
 
 module.exports = router;
