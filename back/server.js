a696ef87c9d9d7052d230b355cc5d2962ba2ef29
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-23 05:07:21 +0100

    added socket.io

--- back/server.js
+++ back/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+var http = require('http').Server(app);
 const dbConnect = require('./config/db');
 const cors = require('cors');
 const config = require('config');
 const cookieParser = require('cookie-parser');
 const authRoute = require('./routes/api/auth');
 const userRoute = require('./routes/api/user');
+
+var io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+});
 
 dbConnect();
 
@@ -18,6 +26,33 @@ app.use('/api/user/', userRoute);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`)
+
+
+let interval = [];
+
+io.on("connection", (socket) => {
+
+  console.log("New client connected "+socket.id);
+
+  if (interval[socket.id]) {
+    clearInterval(interval[socket.id]);
+  }
+
+  interval[socket.id] = setInterval(() => getApiAndEmit(socket), 1000);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected "+socket.id);
+    clearInterval(interval[socket.id]);
+  });
+
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+http.listen(PORT, () => {
+    console.log(`Server app is listening on port ${PORT}`);
 });
